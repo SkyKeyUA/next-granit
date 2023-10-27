5b184a591437d13b844bdd31d44544ac132f7859
SkyKeyUA <veremiienkoov96@gmail.com>
2023-10-26 20:29:28 -0600

    css was fixed and created half footer

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -14,13 +14,33 @@ export const Footer: React.FC = () => {
   return (
     <footer className={styles.root}>
       <div className={styles.container}>
-        <div className={styles.inner}>
-          <Link href="/" className={styles.logo}>
-            <SvgIcon src={IconsEnum.logo} style={{ width: '275px', height: '27px' }} />
-          </Link>
+        <div className={styles.body}>
+          <div className={styles.inner}>
+            <Link href="/" className={styles.logo}>
+              <SvgIcon src={IconsEnum.logo} style={{ width: '275px', height: '27px' }} />
+            </Link>
+            <div className={styles.title}>
+              <Link href="/">
+                <SvgIcon
+                  src={IconsEnum.location}
+                  style={{ height: '25px', width: '17', marginRight: '15px' }}
+                />
+                <span>м. Бориспіль, вул. Січнева 40Б</span>
+              </Link>
+            </div>
+            <ul className={styles.list}>
+              {phoneNumber.map((obj, index) => (
+                <li className={styles.phone} key={index}>
+                  <Link href={`tel:${obj.fullNumber}`}>
+                    <SvgIcon src={IconsEnum.phone} size={25} style={{ marginRight: '15px' }} />
+                    <span>{obj.shortNumber}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.inner}>Maps</div>
         </div>
-        <div className={styles.inner}>Наші вироби</div>
-        <div className={styles.inner}>Послуги</div>
       </div>
     </footer>
   );
