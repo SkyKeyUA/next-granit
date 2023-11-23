feea775f50b05e94cea35f7e4ee111ce48c3cd20
SkyKeyUA <veremiienkoov96@gmail.com>
2023-11-22 23:25:57 -0600

    create CatalogToggle for Tablet

--- client/src/components/UI/Hamburger/index.tsx
+++ client/src/components/UI/Hamburger/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 
 import styles from './Hamburger.module.scss';
 import { HamburgerProps } from './Hamburger.types';
+import { useAppDispatch } from '@hooks/redux';
+import { setCategoryToggle } from '@redux/reducers/filter/reducer';
 
 export const Hamburger: React.FC<HamburgerProps> = ({ setMenuOpen, menuOpen }) => {
+  const dispatch = useAppDispatch();
+  const handleButtonClick = () => {
+    dispatch(setCategoryToggle(false));
+    setMenuOpen(!menuOpen);
+  };
   return (
     <button
-      onClick={() => setMenuOpen(!menuOpen)}
+      onClick={handleButtonClick}
       type="button"
       className={menuOpen ? `${styles['icon-menu']} ${styles['menu-open']}` : styles['icon-menu']}>
       <span></span>
