200350059bb7df3148f26b2198004799c6226a18
SkyKeyUA <veremiienkoov96@gmail.com>
2023-12-10 22:20:12 -0600

    upload and getAll products were created

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { ProductModule } from './product/product.module';
+import { FileModule } from './file/file.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_URL),
     UsersModule,
+    ProductModule,
+    FileModule,
   ],
 })
 export class AppModule {}
--- server/src/product/dto/create-product.dto.ts
+++ server/src/product/dto/create-product.dto.ts
@@ -1,8 +1,6 @@
 class CreateProductDto {
-  readonly image: string;
   readonly title: string;
   readonly id: string;
-  readonly subcatalog_id: string;
 }
 
 export { CreateProductDto };
