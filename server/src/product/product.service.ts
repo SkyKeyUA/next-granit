5f870d10036e88086456393ba72f18cedc415c0d
SkyKeyUA <veremiienkoov96@gmail.com>
2023-12-11 21:16:45 -0600

    getOne and update were created

--- server/src/product/product.service.ts
+++ server/src/product/product.service.ts
@@ -5,6 +5,7 @@ import { Model } from 'mongoose';
 import { FileService } from 'src/file/file.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { FileType } from 'src/file/file.interface';
+import { ObjectId } from 'mongodb';
 
 @Injectable()
 export class ProductService {
@@ -23,5 +24,15 @@ export class ProductService {
   async getAll(count = 10, offset = 0): Promise<Product[]> {
     const products = await this.productModel.find().skip(offset).limit(count);
     return products;
+  }
+
+  async getOne(id: ObjectId): Promise<Product> {
+    const product = await this.productModel.findById(id);
+    return product;
+  }
+
+  async update(id: ObjectId, updateData: Partial<CreateProductDto>) {
+    const product = await this.productModel.updateOne({ _id: id }, updateData);
+    return updateData;
   }
 }
