cf5548d13b01b933c1abd136589c8a320661b17e
SkyKeyUA <veremiienkoov96@gmail.com>
2024-01-10 00:02:37 -0600

    addRole was created

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schemas';
 import { Model } from 'mongoose';
 import { RolesService } from 'src/roles/roles.service';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -14,7 +16,7 @@ export class UsersService {
   ) {}
 
   async create(dto: CreateUserDto): Promise<User> {
-    const role = await this.roleService.getByValue('ADMIN');
+    const role = await this.roleService.getByValue('USER');
     const user = await this.usersModule.create({ ...dto, roles: role });
     return user;
   }
@@ -28,6 +30,19 @@ export class UsersService {
     const user = this.usersModule.findOne({ email });
     return user;
   }
+
+  async addRole(dto: AddRoleDto) {
+    const user = await this.usersModule.findById(dto.userId);
+    const role = await this.roleService.getByValue(dto.value);
+    if (role && user) {
+      await user.roles.push(role);
+      console.log(user);
+      return user;
+    }
+    throw new HttpException('User or Role is not found', HttpStatus.NOT_FOUND);
+  }
+
+  async banUser(dto: BanUserDto) {}
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
