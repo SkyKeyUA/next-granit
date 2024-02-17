b3acf6a9b056907e4dfe2beb7ae630aa01baaa3d
SkyKeyUA <veremiienkoov96@gmail.com>
2024-02-16 20:41:55 -0700

    get Products was created

--- client/src/redux/reducers/products/products.reducer.ts
+++ client/src/redux/reducers/products/products.reducer.ts
@@ -1,30 +1,37 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { Products, ProductsSliceState, Status } from './type';
+import { ProductsSliceState, Status } from './products.types';
+import { IProduct } from '@customTypes/index';
+import { fetchProductsPages } from './products.asyncActions';
 
 const initialState: ProductsSliceState = {
   products: [],
   statusProducts: Status.LOADING,
+  currentPage: 0,
+  count: 0,
 };
 
 const productsReducer = createSlice({
   name: 'products',
   initialState,
   reducers: {
-    setProducts(state, action: PayloadAction<Products[]>) {
+    setProducts(state, action: PayloadAction<IProduct[]>) {
       state.products = action.payload;
     },
   },
   extraReducers: (builder) => {
     builder.addCase(fetchProductsPages.pending, (state) => {
       state.products = [];
+      state.count = 0;
       state.statusProducts = Status.LOADING;
     });
     builder.addCase(fetchProductsPages.fulfilled, (state, action) => {
-      state.products = action.payload.products;
+      state.products = action.payload.rows;
+      state.count = action.payload.count;
       state.statusProducts = Status.SUCCESS;
     });
     builder.addCase(fetchProductsPages.rejected, (state) => {
       state.products = [];
+      state.count = 0;
       state.statusProducts = Status.ERROR;
       console.log('There was an error');
     });
