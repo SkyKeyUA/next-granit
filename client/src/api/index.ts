c11fcb4367e41ae4f8ffe7e0f04d70349ff3ad62
SkyKeyUA <veremiienkoov96@gmail.com>
2024-02-21 19:19:16 -0700

    remotePatterns was added in next.config

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEST_APP_API_URL;
+export const baseURL = process.env.NEST_APP_API_URL;
 
 const $api = axios.create({
   withCredentials: true,
