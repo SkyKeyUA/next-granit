a62ae709bcf395f06162aeb55c7ba17b73b39adc
SkyKeyUA <veremiienkoov96@gmail.com>
2024-02-22 20:37:23 -0700

    add pagination

--- client/src/components/UI/Pagination/Pagination.component.tsx
+++ client/src/components/UI/Pagination/Pagination.component.tsx
@@ -5,10 +5,11 @@ import styles from './Pagination.module.scss';
 import { PaginationProps } from './Pagination.types';
 
 export const PaginationComponent: React.FC<PaginationProps> = ({
-  totalPages,
+  count,
   currentPage,
   onChangePage,
 }) => {
+  const totalPages = Math.ceil(count / 9);
   return (
     <ReactPaginate
       className={styles.root}
--- client/src/views/HomePage/HomePage.component.tsx
+++ client/src/views/HomePage/HomePage.component.tsx
@@ -5,21 +5,29 @@ import { Product } from '@components/Product';
 import { useAppDispatch } from '@hooks/redux';
 import { fetchProductsPages } from '@redux/reducers/products/products.asyncActions';
 import { useProductsSelector } from '@redux/reducers/products/products.selectors';
+import { Pagination } from '@components/UI/Pagination';
+import { setCurrentPage } from '@redux/reducers/filter/filter.reducer';
 
 export const HomePageComponent: React.FC = () => {
   const dispatch = useAppDispatch();
-  const { currentPage, products } = useProductsSelector();
+  const { currentPage, count, products } = useProductsSelector();
   const getProducts = () => {
     dispatch(fetchProductsPages(String(currentPage)));
+  };
+  const onChangePage = (page: number) => {
+    dispatch(setCurrentPage(page));
   };
   React.useEffect(() => {
     getProducts();
   }, []);
   return (
     <div className={styles.container}>
-      {products.map((product) => (
-        <Product key={product.id} {...product} />
-      ))}
+      <Pagination currentPage={currentPage} count={count} onChangePage={onChangePage} />
+      <div className={styles.inner}>
+        {products.map((product) => (
+          <Product key={product.id} {...product} />
+        ))}
+      </div>
     </div>
   );
 };
